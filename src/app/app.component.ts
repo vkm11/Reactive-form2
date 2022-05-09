235ea77b73dc675f95ca42ff437787e4d17247ec
vijaykumar <vijay1111mane@gmail.com>
2022-05-09 18:41:06 +0530

    commit;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
   // title = 'Reactive-form';
   constructor(private router:Router){
     
